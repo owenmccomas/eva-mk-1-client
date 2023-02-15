8316b0af4db12772295e5ab3b6a3c68b40639501
Owen McComas <owen@mccomas.me>
2023-02-15 10:42:53 -0700

    initial commit

---
+++ eva-mk1-client/src/App.jsx
@@ -0,0 +1,22 @@
+import './eva.css'
+import React, { useState } from 'react';
+
+function AudioRecorder() {
+  const [isRecording, setIsRecording] = useState(false);
+
+  const handleRecordClick = () => {
+    setIsRecording(!isRecording);
+    // TODO: implement logic for recording audio
+  };
+
+  return (
+    <div>
+      <button onClick={handleRecordClick}>
+        {isRecording ? 'Stop Talking' : 'Start Talking'}
+      </button>
+      <p>Text output goes here</p>
+    </div>
+  );
+}
+
+export default AudioRecorder;
